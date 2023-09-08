be20842f9e3a556a5c597cd9151286b9bb772596
David Rawson <drawers@users.noreply.github.com>
2023-09-08 22:42:37 +1200

    Delete comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,18 +87,5 @@ subprojects {
             publishToMavenCentral(host = SonatypeHost.S01)
             signAllPublications()
         }
-
-        /**
-         * Congratulations! Welcome to the Central Repository!
-         *
-         * Publish snapshot and release artifacts to s01.oss.sonatype.org
-         * Have a look at this section of our official guide for deployment instructions:
-         * https://central.sonatype.org/publish/publish-guide/#deployment
-         *
-         * Depending on your build configuration, your first component(s) might be released automatically after a successful deployment.
-         * If that happens, you will see a comment on this ticket confirming that your artifact has synced to Maven Central.
-         * If you do not see this comment within an hour or two, you can follow the steps in this section of our guide:
-         * https://central.sonatype.org/publish/release/
-         */
     }
 }
